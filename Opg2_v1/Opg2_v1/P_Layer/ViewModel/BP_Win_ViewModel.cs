0f4f9ab598e2a9d51e2d1e6257face23394d95cf
Mads Emil Nielsen <au636837@uni.au.dk>
2020-03-12 23:49:33 +0100

    Graf til Blodtryk simi done, Men jeg kunne godt tænke med at tilføje notify for at gøre det mere dynamisk

--- Opg2_v1/Opg2_v1/P_Layer/ViewModel/BP_Win_ViewModel.cs
+++ Opg2_v1/Opg2_v1/P_Layer/ViewModel/BP_Win_ViewModel.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using System.Windows.Input;
 using Opg2_v1.View;
 using Prism.Commands;
 using Opg2_v1.Interface;
 using GalaSoft.MvvmLight.Command;
 using Logic_tier;
+
+using LiveCharts;
+using LiveCharts.Wpf;
+using DTO;
 
 namespace Opg2_v1.ViewModel
 {
@@ -23,7 +28,8 @@ namespace Opg2_v1.ViewModel
             logic_Lay.WindowCounter++;
 
 
-        WindowCount = logic_Lay.WindowCounter;
+            WindowCount = logic_Lay.WindowCounter;
+            grafLoad();
         }
 
         #region Open HP Window Commands
@@ -54,6 +60,38 @@ namespace Opg2_v1.ViewModel
             HP_Window.Show();
 
         }
+        #endregion
+
+        #region Chart
+        public SeriesCollection BlodP { get; set; }
+        public String[] Labels { get; set; }
+        List<String> labelL;
+
+        ColumnSeries SystolicLine;
+        ColumnSeries DiastolicLine;
+        void grafLoad()
+        { 
+            BlodP = new SeriesCollection();
+            SystolicLine = new ColumnSeries {Title= "Systolic", Values = new ChartValues<int>() };
+            DiastolicLine = new ColumnSeries { Title = "Diastolic", Values = new ChartValues<int>() };
+            labelL = new List<string>();
+            foreach (DTO_BPressure item in logic_Lay.getBPressureData(""))
+            {
+                SystolicLine.Values.Add(item.Systolic_);
+                DiastolicLine.Values.Add(item.Diastolic_);
+                labelL.Add(item.Date_.ToString().Substring(0,16));
+            }
+           
+             Labels = labelL.ToArray();
+         
+            BlodP.Add(DiastolicLine);
+            BlodP.Add(SystolicLine);
+            
+
+        }
+        /*Series="{Binding BlodP}"
+         * Grid.Column="0" Grid.Row="1" Grid.RowSpan="2"
+         * */
         #endregion
 
     }
