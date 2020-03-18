bea3d9f28312a689f2a3cd50804a579820ed9b25
Mads Emil Nielsen <au636837@uni.au.dk>
2020-03-18 09:56:30 +0100

    Kan ikke kalde en .Hide() på mit view..

---
+++ Opg2_V2/P_Lay/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using P_Lay;
+using P_Lay.Views;
+
+namespace P_Lay.ViewModels
+{
+   public class HomePageViewModel
+    {
+        LoginView loginWindow;
+
+
+        #region Commands
+
+        ICommand _calcBMICommand;
+        public ICommand CalcBMICommand
+        {
+            get { return _calcBMICommand ?? (_calcBMICommand = new RelayCommand(CalcBMI, CalcBMICanExecute)); }
+        }
+
+        private void CalcBMI()
+        {
+            loginWindow = new LoginView();
+            loginWindow.ShowDialog();
+        }
+
+        private bool CalcBMICanExecute()
+        {
+            
+                return true;
+         
+        }
+
+        #endregion
+    }
+}
---
+++ Opg2_V2/P_Layer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using MyCommands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using P_Layer.Views;
+
+namespace P_Layer.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        LoginView loginWindow;
+
+
+
+
+        #region Commands
+
+        ICommand _calcBMICommand;
+        public ICommand CalcBMICommand
+        {
+            get { return _calcBMICommand ?? (_calcBMICommand = new RelayCommand(CalcBMI, CalcBMICanExecute)); }
+        }
+
+        private void CalcBMI()
+        {
+            loginWindow = new LoginView();
+            
+            loginWindow.ShowDialog();
+        }
+
+        private bool CalcBMICanExecute()
+        {
+            return true;
+        }
+
+        #endregion
+    }
+}
